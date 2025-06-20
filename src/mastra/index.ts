3a976c4f3895aca9c0bf86b03f91103e30bd2f24
David Britt <djmb102@gmail.com>
2025-06-20 12:29:08 +0200

    add yourkagent to index

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,17 +1,12 @@
 
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
-import { LibSQLStore } from '@mastra/libsql';
-import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
+import { yourAgent } from './agents/your-agent'
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
-  agents: { weatherAgent },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
+  agents: { weatherAgent, yourAgent },
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
